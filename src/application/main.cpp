c9c29a82c14fd885861a145f22a51705bf481dfb
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-07-15 16:46:31 +0200

    feat: organize main.cpp code

--- src/application/main.cpp
+++ src/application/main.cpp
@@ -12,45 +12,60 @@
 #include <QQuickStyle>
 #include <QSurfaceFormat>
 
+#include <QDir>
+#include <QDirIterator>
+
 /* ************************************************************************** */
 
-int main(int argc, char *argv[])
-{
-    // Hacks ///////////////////////////////////////////////////////////////////
 
-#if defined(Q_OS_LINUX) && !defined(Q_OS_ANDROID)
-    // NVIDIA driver suspend&resume hack
-    auto format = QSurfaceFormat::defaultFormat();
-    format.setOption(QSurfaceFormat::ResetNotification);
-    QSurfaceFormat::setDefaultFormat(format);
-#endif
+void logQtResourceDirectories(QQmlApplicationEngine engine) {
+    qDebug() << engine.importPathList();
+    QDirIterator qrc(":", QDirIterator::Subdirectories);
+    while(qrc.hasNext()) { qWarning() << qrc.next(); }
+};
 
+void runHacks() {
+    #if defined(Q_OS_LINUX) && !defined(Q_OS_ANDROID)
+        // NVIDIA driver suspend&resume hack
+        auto format = QSurfaceFormat::defaultFormat();
+        format.setOption(QSurfaceFormat::ResetNotification);
+        QSurfaceFormat::setDefaultFormat(format);
+    #endif
+
+    #if !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS)
+        // Qt 6.6+ mouse wheel hack
+        qputenv("QT_QUICK_FLICKABLE_WHEEL_DECELERATION", "10000");
+    #endif
+};
+
+int showWindowFromRootElement(QQmlApplicationEngine engine) {
+    // only create window in desktop
 #if !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS)
-    // Qt 6.6+ mouse wheel hack
-    qputenv("QT_QUICK_FLICKABLE_WHEEL_DECELERATION", "10000");
+    // QQuickWindow must be valid at this point
+    QQuickWindow *window = qobject_cast<QQuickWindow *>(engine.rootObjects().value(0));
+    if (!window) return EXIT_FAILURE;
 #endif
 
-    // Force OpenGL for Qt WebEngine or the MapLibre plugin
-    //qputenv("QSG_RHI_BACKEND", "opengl");
+    return 0;
+}
 
-    // DEBUG ///////////////////////////////////////////////////////////////////
+void createMacOSDock() {
+#if defined(Q_OS_MACOS)
+    // macOS dock
+    MacOSDockHandler *dockIconHandler = MacOSDockHandler::getInstance();
+    QObject::connect(dockIconHandler, &MacOSDockHandler::dockIconClicked, window, &QQuickWindow::show);
+    QObject::connect(dockIconHandler, &MacOSDockHandler::dockIconClicked, window, &QQuickWindow::raise);
+#endif
+};
 
-    //qputenv("QSG_INFO", "1");               // print Qt Scene Graph info
-    //qputenv("QT_QPA_EGLFS_DEBUG", "1");     // print Qt Platform Abstraction EGL debug info
-    //qputenv("QT_DEBUG_PLUGINS", "1");       // print Qt plugins info
-
-    // Force QtQuick components style? // Some styles are only available on target OS
-    // Basic // Fusion // Imagine // macOS // iOS // Material // Universal // Windows
-    //QQuickStyle::setStyle("Universal");
-
+int createApplication(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     engine.loadFromModule("AdvancedEffects", "Main");
 
-    //qDebug() << engine.importPathList();
-    //QDirIterator qrc(":", QDirIterator::Subdirectories);
-    //while(qrc.hasNext()) { qWarning() << qrc.next(); }
+    // Comment this out for debugging:
+    //logQtResourceDirectories(&engine);
 
     if (engine.rootObjects().isEmpty())
     {
@@ -58,22 +73,20 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-#if !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS) // desktop section
+    int error = showWindowFromRootElement(&engine);
+    if (error != 0) {
+        return error;
+    }
 
-    // QQuickWindow must be valid at this point
-    QQuickWindow *window = qobject_cast<QQuickWindow *>(engine.rootObjects().value(0));
-    if (!window) return EXIT_FAILURE;
-
-#if defined(Q_OS_MACOS)
-    // macOS dock
-    MacOSDockHandler *dockIconHandler = MacOSDockHandler::getInstance();
-    QObject::connect(dockIconHandler, &MacOSDockHandler::dockIconClicked, window, &QQuickWindow::show);
-    QObject::connect(dockIconHandler, &MacOSDockHandler::dockIconClicked, window, &QQuickWindow::raise);
-#endif
-
-#endif // desktop section
-
+    createMacOSDock();
     return app.exec();
+};
+
+int main(int argc, char *argv[])
+{
+    runHacks();
+
+    return createApplication(argc, argv);
 }
 
 /* ************************************************************************** */
