ba4da378eb1e167e44a066f20ca17feb4f43d3a5
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-07-26 16:04:20 +0200

    feat: do import accessibility module

--- src/application/main.cpp
+++ src/application/main.cpp
@@ -4,21 +4,29 @@
 #include <QApplication>
 #include <QDirIterator>
 
+#include "accessibility/accessibilitymodule.h"
 #include "app.h"
 #include "cmdline.h"
 
+// Application modules!!
+#include "accessibility/accessibilitymodule.h"
 #include "ui/uimodule.h"
+#include "dockwindow/dockmodule.h"
 #include "uicomponents/uicomponentsmodule.h"
 #include "appshellmodule.h"
 
 using namespace ae::app;
 
 int main(int argc, char *argv[]) {
+    /* ================ Load application modules ================ */
     Application application;
+    application.addModule(new muse::accessibility::AccessibilityModule());
     application.addModule(new muse::ui::UiModule());
     application.addModule(new muse::uicomponents::UiComponentsModule());
+    application.addModule(new muse::dock::DockModule());
     application.addModule(new ae::appshell::AppShellModule());
 
+    /* ================ Setup variables ================ */
     CommandLineParser parser;
     parser.init();
     parser.parse(argc, argv);
@@ -36,5 +44,6 @@ int main(int argc, char *argv[]) {
         while(qrc.hasNext()) { qWarning() << qrc.next(); }
     };
 
+    /* ================ Run application ================ */
     return application.exec(parser, qtApp, engine);
 };
