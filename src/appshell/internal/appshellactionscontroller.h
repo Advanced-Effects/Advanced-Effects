bbdfe810b1186336871deaffa8a2bf5fa686626d
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-09-06 00:08:25 +0200

    feat: implement actions succesfully
    by: - implementing IUiActionsModule - appending it to IUiActionsRegister
    in appshellmodule

---
+++ src/appshell/internal/appshellactionscontroller.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "modularity/ioc.h"
+#include "actions/iactionsdispatcher.h"
+#include "actions/actionable.h"
+#include "iinteractive.h"
+
+namespace app::appshell {
+
+using namespace muse;
+
+class AppshellActionController : public Injectable, public actions::Actionable
+{
+        Inject<actions::IActionsDispatcher> dispatcher = { this };
+        Inject<IInteractive> interactive = { this };
+
+public:
+        AppshellActionController(const modularity::ContextPtr& iocCtx)
+        : Injectable(iocCtx) {}
+
+        void init();
+};
+
+};
