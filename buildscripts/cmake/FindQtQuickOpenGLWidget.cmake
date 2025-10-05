c135161a76c26bd8e09304d3e8257bab62ee7212
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-10-05 10:27:45 +0200

    fix: missing arguments for QtQuickOpenGLWidget
    Add missing arguments to ExternalProject_Add in order to download
    QtQuickOpenGLWidget

--- buildscripts/cmake/FindQtQuickOpenGLWidget.cmake
+++ buildscripts/cmake/FindQtQuickOpenGLWidget.cmake
@@ -6,4 +6,11 @@ include(ExternalProject)
 ExternalProject_Add(QtQuickOpenGLWidget
        GIT_REPOSITORY https://github.com/advanced-effects/qtQuick-OpenGL-Widget.git
        GIT_TAG main
+
+       BUILD_COMMAND ${CMAKE_COMMAND} --build .
+       INSTALL_COMMAND ${CMAKE_COMMAND} --install .
+
+       # ! NOTE: We share the CMAKE_PREFIX_PATH with QtQuickOpenGLWidget
+       # So that it can find Qt also (when it's installed to a custom location)
+       CMAKE_ARGS -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
 )
