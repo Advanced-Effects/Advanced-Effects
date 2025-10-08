8ab38fbf5d893e8f905184f84db9cccdd631338d
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-10-08 21:15:15 +0200

    feat: add preferences dialog

---
+++ src/appshell/view/preferences/preferencesmodel.cpp
@@ -0,0 +1,276 @@
+#include <QApplication>
+
+#include "preferencesmodel.h"
+
+#include "translation.h"
+#include "ui/view/iconcodes.h"
+
+#include "log.h"
+
+using namespace mu::appshell;
+using namespace muse::ui;
+
+PreferencesModel::PreferencesModel(QObject* parent)
+    : QAbstractItemModel(parent), muse::Injectable(muse::iocCtxForQmlObject(this))
+{
+}
+
+PreferencesModel::~PreferencesModel()
+{
+    cancel();
+
+    delete m_rootItem;
+    m_rootItem = nullptr;
+}
+
+QModelIndex PreferencesModel::index(int row, int column, const QModelIndex& parent) const
+{
+    if (!hasIndex(row, column, parent)) {
+        return QModelIndex();
+    }
+
+    PreferencePageItem* parentItem = nullptr;
+
+    if (!parent.isValid()) {
+        parentItem = m_rootItem;
+    } else {
+        parentItem = modelIndexToItem(parent);
+    }
+
+    if (!parentItem) {
+        return QModelIndex();
+    }
+
+    PreferencePageItem* childItem = parentItem->childAtRow(row);
+
+    if (childItem) {
+        return createIndex(row, column, childItem);
+    }
+
+    return QModelIndex();
+}
+
+QModelIndex PreferencesModel::parent(const QModelIndex& child) const
+{
+    PreferencePageItem* childItem = modelIndexToItem(child);
+    if (!childItem) {
+        return QModelIndex();
+    }
+
+    PreferencePageItem* parentItem = qobject_cast<PreferencePageItem*>(childItem->parentItem());
+
+    if (parentItem == m_rootItem) {
+        return QModelIndex();
+    }
+
+    return createIndex(parentItem->row(), 0, parentItem);
+}
+
+int PreferencesModel::rowCount(const QModelIndex& parent) const
+{
+    PreferencePageItem* parentItem = nullptr;
+
+    if (!parent.isValid()) {
+        parentItem = m_rootItem;
+    } else {
+        parentItem = modelIndexToItem(parent);
+    }
+
+    if (!parentItem) {
+        return 0;
+    }
+
+    return parentItem->childCount();
+}
+
+int PreferencesModel::columnCount(const QModelIndex&) const
+{
+    return 1;
+}
+
+QVariant PreferencesModel::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid() && role != ItemRole) {
+        return QVariant();
+    }
+
+    PreferencePageItem* item = modelIndexToItem(index);
+
+    if (!item) {
+        return QVariant();
+    }
+
+    return QVariant::fromValue(qobject_cast<QObject*>(item));
+}
+
+QHash<int, QByteArray> PreferencesModel::roleNames() const
+{
+    return { { ItemRole, "itemRole" } };
+}
+
+QString PreferencesModel::currentPageId() const
+{
+    return m_currentPageId;
+}
+
+void PreferencesModel::load(const QString& currentPageId)
+{
+    configuration()->startEditSettings();
+
+    beginResetModel();
+
+    if (!currentPageId.isEmpty()) {
+        setCurrentPageId(currentPageId);
+    } else {
+        const QString& lastOpenedPageId = configuration()->preferencesDialogLastOpenedPageId();
+        if (lastOpenedPageId.isEmpty()) {
+            setCurrentPageId("general");
+        } else {
+            setCurrentPageId(lastOpenedPageId);
+        }
+    }
+
+    m_rootItem = new PreferencePageItem();
+
+    QList<PreferencePageItem*> items {
+        makeItem("general", QT_TRANSLATE_NOOP("appshell/preferences", "General"), IconCode::Code::SETTINGS_COG,
+                 "Preferences/GeneralPreferencesPage.qml"),
+
+        makeItem("appearance", QT_TRANSLATE_NOOP("appshell/preferences", "Appearance"), IconCode::Code::EYE_OPEN,
+                 "Preferences/AppearancePreferencesPage.qml"),
+    };
+
+    for (PreferencePageItem* item: items) {
+        m_rootItem->appendChild(item);
+    }
+
+    endResetModel();
+}
+
+bool PreferencesModel::askForConfirmationOfPreferencesReset()
+{
+    std::string title = muse::trc("appshell", "Are you sure you want to reset preferences?");
+    std::string question = muse::trc("appshell", "This action will reset all your app preferences and delete all custom shortcuts. "
+                                                 "It will not delete any of your scores.\n\n"
+                                                 "This action cannot be undone.");
+
+    muse::IInteractive::ButtonData cancelBtn = interactive()->buttonData(muse::IInteractive::Button::Cancel);
+    muse::IInteractive::ButtonData resetBtn = interactive()->buttonData(muse::IInteractive::Button::Reset);
+    cancelBtn.accent = true;
+
+    muse::IInteractive::Result result = interactive()->warningSync(title, question, { cancelBtn, resetBtn }, cancelBtn.btn,
+                                                                   { muse::IInteractive::Option::WithIcon },
+                                                                   muse::trc("appshell", "Reset preferences"));
+    return result.standardButton() == muse::IInteractive::Button::Reset;
+}
+
+void PreferencesModel::resetFactorySettings()
+{
+    static constexpr bool KEEP_DEFAULT_SETTINGS = true;
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    QApplication::processEvents();
+    configuration()->revertToFactorySettings(KEEP_DEFAULT_SETTINGS);
+
+    // Unreset the "First Launch Completed" setting so the first-time launch wizard does not appear.
+    configuration()->setHasCompletedFirstLaunchSetup(true);
+
+    configuration()->startEditSettings();
+    QApplication::restoreOverrideCursor();
+}
+
+void PreferencesModel::apply()
+{
+    configuration()->applySettings();
+}
+
+void PreferencesModel::cancel()
+{
+    configuration()->rollbackSettings();
+}
+
+void PreferencesModel::selectRow(const QModelIndex& rowIndex)
+{
+    QModelIndex parentItemIndex = parent(rowIndex);
+    PreferencePageItem* parentItem = nullptr;
+    if (!parentItemIndex.isValid()) {
+        parentItem = m_rootItem;
+    } else {
+        parentItem = modelIndexToItem(parentItemIndex);
+    }
+
+    QList<PreferencePageItem*> children = parentItem->childrenItems();
+    for (PreferencePageItem* child: children) {
+        child->setExpanded(false);
+    }
+
+    PreferencePageItem* selectedItem = parentItem->childAtRow(rowIndex.row());
+    if (!selectedItem) {
+        return;
+    }
+
+    selectedItem->setExpanded(true);
+    setCurrentPageId(selectedItem->id());
+}
+
+QVariantList PreferencesModel::availablePages() const
+{
+    std::function<QVariantList(const PreferencePageItem*)> childPages;
+    childPages = [&childPages](const PreferencePageItem* item) {
+        QVariantList result;
+
+        for (int i = 0; i < item->childCount(); ++i) {
+            PreferencePageItem* child = item->childAtRow(i);
+            QVariantMap childObj;
+            childObj["id"] = child->id();
+            childObj["path"] = child->path();
+            result << childObj;
+
+            QVariantList pages = childPages(child);
+            for (const QVariant& page: pages) {
+                result << page;
+            }
+        }
+
+        return result;
+    };
+
+    return childPages(m_rootItem);
+}
+
+void PreferencesModel::setCurrentPageId(QString currentPageId)
+{
+    if (m_currentPageId == currentPageId) {
+        return;
+    }
+
+    m_currentPageId = currentPageId;
+    configuration()->setPreferencesDialogLastOpenedPageId(currentPageId);
+    emit currentPageIdChanged(m_currentPageId);
+}
+
+PreferencePageItem* PreferencesModel::makeItem(const QString& id, const QString& title, muse::ui::IconCode::Code icon,
+                                               const QString& path,
+                                               const QList<PreferencePageItem*>& children) const
+{
+    PreferencePageItem* item = new PreferencePageItem();
+    item->setId(id);
+    item->setTitle(title);
+    item->setIcon(icon);
+    item->setPath(path);
+    item->setExpanded(id == currentPageId());
+
+    for (PreferencePageItem* child: children) {
+        item->appendChild(child);
+
+        if (child->id() == currentPageId()) {
+            item->setExpanded(true);
+        }
+    }
+
+    return item;
+}
+
+PreferencePageItem* PreferencesModel::modelIndexToItem(const QModelIndex& index) const
+{
+    return static_cast<PreferencePageItem*>(index.internalPointer());
+}
