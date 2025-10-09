aaf8ec789683b31813e2f5e582c58b49e7f556d8
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-10-09 21:32:01 +0200

    feat: blank gl canvas succesfully showing!

--- src/appshell/view/canvas/applicationcanvas.cpp
+++ src/appshell/view/canvas/applicationcanvas.cpp
@@ -8,7 +8,18 @@ QQuickFramebufferObject::Renderer *ApplicationCanvas::createRenderer() const {
         return new Renderer;
 };
 
-void ApplicationCanvas::Renderer::render() {};
+void ApplicationCanvas::Renderer::render() {
+        auto fbo = framebufferObject();
+        if (!fbo) return;
+
+        fbo->bind();
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        fbo->release();
+        update();
+};
 
 QOpenGLFramebufferObject *ApplicationCanvas::Renderer::createFramebufferObject(const QSize &size) {
         return new QOpenGLFramebufferObject(size);
