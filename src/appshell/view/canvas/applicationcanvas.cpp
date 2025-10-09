520a3fa042ccedf2d83569377182c25606c95801
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-10-09 20:48:57 +0200

    feat: turn AppCanvas into a FBO (OpenGL texture)

--- src/appshell/view/canvas/applicationcanvas.cpp
+++ src/appshell/view/canvas/applicationcanvas.cpp
@@ -2,4 +2,14 @@
 
 using namespace app::appshell;
 
-ApplicationCanvas::ApplicationCanvas(QQuickItem *parent) : QQuickItem(parent) {};
+ApplicationCanvas::ApplicationCanvas(QQuickItem *parent) : QQuickFramebufferObject(parent) {};
+
+QQuickFramebufferObject::Renderer *ApplicationCanvas::createRenderer() const {
+        return new Renderer;
+};
+
+void ApplicationCanvas::Renderer::render() {};
+
+QOpenGLFramebufferObject *ApplicationCanvas::Renderer::createFramebufferObject(const QSize &size) {
+        return new QOpenGLFramebufferObject(size);
+};
--- src/appshell/view/canvas/applicationcanvas.h
+++ src/appshell/view/canvas/applicationcanvas.h
@@ -1,14 +1,26 @@
 #pragma once
 
-#include <QQuickItem>
+#include <QQuickFramebufferObject>
+#include <QOpenGLFramebufferObject>
 
 namespace app::appshell {
 
-class ApplicationCanvas : public QQuickItem {
+class ApplicationCanvas : public QQuickFramebufferObject {
         Q_OBJECT
 public:
         ApplicationCanvas(QQuickItem *parent = nullptr);
         ~ApplicationCanvas() = default;
+
+public:
+        ApplicationCanvas::Renderer *createRenderer() const override;
+
+        class Renderer : public QQuickFramebufferObject::Renderer {
+        public:
+                // OpenGL rendering code goes here
+                void render() override;
+
+                QOpenGLFramebufferObject *createFramebufferObject(const QSize &size) override;
+        };
 };
 
 };
