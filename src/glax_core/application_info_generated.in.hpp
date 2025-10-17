c61730a282c933317bab78e090c54f5670db1355
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-10-17 19:55:04 +0200

    feat: download glaxnimate core

---
+++ src/glax_core/application_info_generated.in.hpp
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2023 Mattia Basaglia <dev@dragon.best>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#define PROJECT_VERSION "${PROJECT_VERSION}"
+#define PROJECT_SLUG "${PROJECT_SLUG}"
+#define PROJECT_NAME "${PROJECT_NAME}"
+#define PROJECT_ID "${PROJECT_ID}"
+#define URL_DOCS "${URL_DOCS}"
+#define URL_ISSUES "${URL_ISSUES}"
+#define URL_DONATE "${URL_DONATE}"
+#define PROJECT_DESCRIPTION "${PROJECT_DESCRIPTION}"
