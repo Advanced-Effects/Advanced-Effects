2ac3de1bc9eb46cc35d849856f51730b7c59c768
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-12-01 17:03:12 +0100

    sketch: canvas drawing code

--- src/projectscene/view/canvas/applicationcanvas.cpp
+++ src/projectscene/view/canvas/applicationcanvas.cpp
@@ -1,5 +1,12 @@
 #include "applicationcanvas.h"
 
+#include <QQuickFramebufferObject>
+#include <QOpenGLFramebufferObject>
+
+#include "include/core/SkImageInfo.h"
+
+#include "glax_core/model/assets/composition.hpp"
+
 using namespace app::projectscene;
 
 ApplicationCanvas::ApplicationCanvas(QQuickItem *parent) : QQuickFramebufferObject(parent) {};
@@ -17,10 +24,48 @@ void ApplicationCanvas::Renderer::render() {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        performSkiaDraw();
+
         fbo->release();
         update();
 };
 
+std::unique_ptr<SkSurface> createSkiaSurfaceForFBO(GLuint fbo, int width, int height) {
+    // Create Skia surface
+    SkImageInfo info = SkImageInfo::MakeN32Premul(width, height);
+    auto backendTexture = SkGr::GetGLBackendTexture();
+
+    return SkSurface::MakeFromBackendTexture(
+        backendTexture,
+        width,
+        height,
+        kTopLeft_GrSurfaceOrigin,
+        /* Sample count */ 1,
+        kPremul_SkAlphaType,
+        nullptr // Do not use a direct pixel copying
+    );
+}
+
+void ApplicationCanvas::Renderer::initSkia(GLuint fbo, int width, int height) {
+        m_skia_surface = createSkiaSurfaceForFBO(fbo, width, height);
+        m_skia_canvas = m_skia_surface->getCanvas();
+};
+
+void ApplicationCanvas::Renderer::performSkiaDraw() {
+        if (!m_skia_canvas) return;
+
+        auto projectFile = globalContext()->currentProjectFile();
+        if (!projectFile) return;
+
+        auto composition = projectFile->current_comp();
+        auto rootNode = composition->docnode_parent();
+        //rootNode->on_paint_skia(m_skia_canvas, ...);
+};
+
 QOpenGLFramebufferObject *ApplicationCanvas::Renderer::createFramebufferObject(const QSize &size) {
-        return new QOpenGLFramebufferObject(size);
+        auto fbo = new QOpenGLFramebufferObject(size);
+        // Link skia to OpenGL Framebuffer Object
+        initSkia(fbo->handle(), size.width(), size.height());
+
+        return fbo;
 };
