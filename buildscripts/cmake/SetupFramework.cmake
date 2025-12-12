d4c62e82db4bcb288bcd494b7c5ee7f6e302417c
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-12-12 23:28:46 +0100

    Maybe try with MakeAvailable

--- buildscripts/cmake/SetupFramework.cmake
+++ buildscripts/cmake/SetupFramework.cmake
@@ -1,16 +1,7 @@
 include(FetchContent)
 
-# Allow using FetchContent_Populate
-cmake_policy(SET CMP0169 OLD)
-
 FetchContent_Declare(Framework
         GIT_REPOSITORY https://github.com/Advanced-Effects/framework.git
         GIT_TAG main)
 
-FetchContent_Populate(Framework)
-
-if (Framework_BINARY_DIR)
-add_subdirectory(${Framework_SOURCE_DIR} ${Framework_BINARY_DIR})
-else()
-add_subdirectory(${Framework_SOURCE_DIR})
-endif()
+FetchContent_MakeAvailable(Framework)
