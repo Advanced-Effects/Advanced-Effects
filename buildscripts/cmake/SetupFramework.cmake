c6acd74db265209661c5e0ca1cb3edb41f95cc04
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-12-12 22:55:59 +0100

    Try pulling framework manually

--- buildscripts/cmake/SetupFramework.cmake
+++ buildscripts/cmake/SetupFramework.cmake
@@ -3,4 +3,6 @@ include(FetchContent)
 FetchContent_Declare(Framework
         GIT_REPOSITORY https://github.com/Advanced-Effects/framework.git
         GIT_TAG main)
-FetchContent_MakeAvailable(Framework)
+
+FetchContent_Populate(Framework)
+add_subdirectory(${Framework_SOURCE_DIR} ${Framework_BINARY_DIR})
